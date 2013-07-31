baa2b61dbdf19f8b638a87e3435da1818a26bf6b
Daniel Bader <mail@dbader.org>
2013-07-30 18:34:07 -0700

    Add StackQueue

--- fifo_queue.py
+++ fifo_queue.py
@@ -1,4 +1,5 @@
 from linked_list import LinkedList
+from stack import Stack
 
 
 class UnderflowException(Exception):
@@ -15,7 +16,7 @@ class Queue:
         return self.items.is_empty
 
     def enqueue(self, x):
-        """O(n)"""
+        """O(n) --> should be O(1)"""
         self.items = self.items.append(x)
 
     def dequeue(self):
@@ -26,3 +27,26 @@ class Queue:
             x = self.items.head
             self.items = self.items.drop(1)
             return x
+
+
+class StackQueue:
+    """A queue implemented using two stacks"""
+    def __init__(self):
+        self.newest = Stack()  # newest elements on top
+        self.oldest = Stack()  # oldest elements on top
+
+    @property
+    def is_empty(self):
+        return self.newest.is_empty and self.oldest.is_empty
+
+    def enqueue(self, x):
+        self.newest.push(x)
+
+    def dequeue(self):
+        if self.is_empty:
+            raise UnderflowException
+        if not self.oldest.is_empty:
+            return self.oldest.pop()
+        while not self.newest.is_empty:
+            self.oldest.push(self.newest.pop())
+        return self.oldest.pop()
--- fifo_queue_test.py
+++ fifo_queue_test.py
@@ -1,5 +1,5 @@
 import pytest
-from fifo_queue import Queue, UnderflowException
+from fifo_queue import Queue, StackQueue, UnderflowException
 
 
 def test_enqueue():
@@ -7,12 +7,31 @@ def test_enqueue():
     assert q.is_empty
     q.enqueue(1)
     q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
     assert not q.is_empty
     assert q.dequeue() == 1
     assert q.dequeue() == 2
+    assert q.dequeue() == 3
+    assert q.dequeue() == 4
     assert q.is_empty
 
 
 def test_dequeue_empty_raises():
     with pytest.raises(UnderflowException):
         Queue().dequeue()
+
+
+def test_stack_queue():
+    q = StackQueue()
+    assert q.is_empty
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
+    assert not q.is_empty
+    assert q.dequeue() == 1
+    assert q.dequeue() == 2
+    assert q.dequeue() == 3
+    assert q.dequeue() == 4
+    assert q.is_empty
